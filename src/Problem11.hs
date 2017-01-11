ac9284ff57e508dfc04a5d200f3a3a17fac019f9
Thomas Oo <thein.oo@mail.mcgill.ca>
2017-01-11 14:20:46 -0500

    Able to eat a number in a grid specified by Problem11.txt

--- src/Problem11.hs
+++ src/Problem11.hs
@@ -9,12 +9,18 @@ eg. numberAt :: (Int,Int) -> Int
 the implementation will search the grid for that number
 -}
 
+main = do
+  contents <- readFile "Problem11.txt"
+  let grid = loadGrid $ lines contents
+  print grid
+  print $ numberAt (4,4) grid
+
 readString :: String -> [Int]
-readString string = map read $ words string
+readString string =  map read $ words string
 
 loadGrid :: [String] -> [[Int]]
 loadGrid grid = map readString grid
 
 --stub.
-numberAt :: (Int,Int) -> Int
-numberAt _ = 0
+numberAt :: (Int,Int) -> [[Int]] -> Int
+numberAt (x,y) grid = (grid!! y) !! x
