799220c73a7b7bb601dab674f35251270bc0840d
Thomas Oo <thein.oo@mail.mcgill.ca>
2017-01-11 14:36:23 -0500

    added methods to calc vertical, horizontal, and diagonal products

--- src/Problem11.hs
+++ src/Problem11.hs
@@ -12,9 +12,14 @@ the implementation will search the grid for that number
 main = do
   contents <- readFile "Problem11.txt"
   let grid = loadGrid $ lines contents
-  print grid
-  print $ numberAt (4,4) grid
-
+  print "Number at (0,0): "
+  print $ numberAt (0,0) grid
+  print "Vertical product of (0,0): "
+  print $ verticalProduct (0,0) grid
+  print "Horizontal product of (0,0): "
+  print $ horizontalProduct (0,0) grid
+  print "Diagonal product of (0,0): "
+  print $ diagonalProduct (0,0) grid
 readString :: String -> [Int]
 readString string =  map read $ words string
 
@@ -24,3 +29,27 @@ loadGrid grid = map readString grid
 --stub.
 numberAt :: (Int,Int) -> [[Int]] -> Int
 numberAt (x,y) grid = (grid!! y) !! x
+
+verticalProduct :: (Int,Int) -> [[Int]]-> Int
+verticalProduct (x,y) grid =
+  let a = numberAt (x,y) grid
+      b = numberAt(x,y+1) grid
+      c = numberAt(x,y+2) grid
+      d = numberAt(x,y+3) grid
+  in a*b*c*d
+
+horizontalProduct :: (Int,Int) -> [[Int]] -> Int
+horizontalProduct (x,y) grid =
+  let a = numberAt (x,y) grid
+      b = numberAt (x+1,y) grid
+      c = numberAt (x+2,y) grid
+      d = numberAt (x+3,y) grid
+  in a*b*c*d
+
+diagonalProduct :: (Int,Int) -> [[Int]] -> Int
+diagonalProduct (x,y) grid =
+  let a = numberAt (x,y) grid
+      b = numberAt (x+1,y+1) grid
+      c = numberAt (x+2,y+2) grid
+      d = numberAt (x+3,y+3) grid
+  in a*b*c*d
